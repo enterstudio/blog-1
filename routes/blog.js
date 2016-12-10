767efcb7a28c2412b773dd1f6704b5c7d1c55789
Adam Onishi <onishiweb@gmail.com>
2016-12-10 19:12:30 +0000

    Use app root path module

--- routes/blog.js
+++ routes/blog.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
+const appRoot = require('app-root-path');
 const marked = require('marked');
 const fs = require('fs');
 
 /* GET home page. */
 router.get('/:postName', function(req, res) {
 	const post = req.params.postName;
-	const path = __dirname + '/../blog/' + post + '.md';
+	const path = appRoot + '/blog/' + post + '.md';
 
 	const file = fs.readFileSync(path, 'utf8');
 
