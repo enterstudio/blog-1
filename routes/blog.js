06d6c5c1fc016666262cd71008739c5230283c88
Adam Onishi <onishiweb@gmail.com>
2016-12-10 23:07:55 +0000

    Add havePost function with Promises

--- routes/blog.js
+++ routes/blog.js
@@ -2,15 +2,28 @@ const router = require('express').Router();
 const appRoot = require('app-root-path');
 const marked = require('marked');
 const fs = require('fs');
+const havePost = require('../lib/blog').havePost;
 
 /* GET home page. */
 router.get('/:postName', function(req, res) {
 	const post = req.params.postName;
-	const path = appRoot + '/blog/' + post + '.md';
+	let file = '';
+	let postPath = '';
 
-	const file = fs.readFileSync(path, 'utf8');
+	havePost(post)
+		.then(function(post) {
+			file = fs.readFileSync(post, 'utf8');
+			res.render('blog', { title: 'Express', body: marked(file.toString()) });
+		})
+		.catch(function(blogError) {
+			const err = new Error('Not Found');
+			res.locals.message = err.message;
+			res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-	res.render('blog', { title: 'Express', body: marked(file.toString()) });
+			// render the error page
+			res.status(err.status || 500);
+			res.render('error');
+		});
 });
 
 module.exports = router;
